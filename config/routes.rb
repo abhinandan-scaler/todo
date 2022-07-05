890ddc94e1d85e84fe66a72293ce3de427723c3e
Abhinandan Mishra <abhinandanmishra360@gmail.com>
2022-07-05 15:06:17 +0530

    Updated the routes and delete function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :todo_lists
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :todo_lists do
+    resources:todo_items do
+      member do
+        patch :complete
+      end
+  end
   root "todo_lists#index"
 end
